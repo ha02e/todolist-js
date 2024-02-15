ef795e302966d90ef1b94195e9e870fbfda4f9a8
hayoung <ha02e@naver.com>
2024-02-15 17:52:21 +0900

    필터 기능 추가

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div");
+
 let taskList = [];
+let filterList = [];
+let mode = "all";
 
 addButton.addEventListener("click", addTask);
+
+for (let i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  });
+}
 
 function addTask() {
   if (taskInput.value == "") {
@@ -15,27 +25,35 @@ function addTask() {
       isComplete: false,
     };
     taskList.push(task);
-    console.log(taskList);
+    //console.log(taskList);
     render();
     taskInput.value = "";
   }
 }
 
 function render() {
+  //내가 선택한 탭에 따라서 리스트를 다르게 보여준다.
+  let list = [];
+  if (mode === "all") {
+    list = taskList;
+  } else if (mode === "ongoing" || mode === "done") {
+    list = filterList;
+  }
+
   let resultHTML = "";
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete == true) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
       resultHTML += `<li class="task">
-      <div class="task-text task-done" onclick="toggleComplete('${taskList[i].id}')">${taskList[i].taskContent}</div>
+      <div class="task-text task-done" onclick="toggleComplete('${list[i].id}')">${list[i].taskContent}</div>
       <div>
-        <button class="delete-btn" onclick="deleteTask('${taskList[i].id}')"></button>
+        <button class="delete-btn" onclick="deleteTask('${list[i].id}')"></button>
       </div>
     </li>`;
     } else {
       resultHTML += `<li class="task">
-      <div class="task-text" onclick="toggleComplete('${taskList[i].id}')">${taskList[i].taskContent}</div>
+      <div class="task-text" onclick="toggleComplete('${list[i].id}')">${list[i].taskContent}</div>
       <div>
-        <button class="delete-btn" onclick="deleteTask('${taskList[i].id}')"></button>
+        <button class="delete-btn" onclick="deleteTask('${list[i].id}')"></button>
       </div>
     </li>`;
     }
@@ -45,7 +63,7 @@ function render() {
 }
 
 function toggleComplete(id) {
-  console.log(id);
+  //console.log(id);
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id == id) {
       taskList[i].isComplete = !taskList[i].isComplete;
@@ -53,7 +71,7 @@ function toggleComplete(id) {
     }
   }
   render();
-  console.log(taskList);
+  //console.log(taskList);
 }
 
 function deleteTask(id) {
@@ -67,6 +85,32 @@ function deleteTask(id) {
     }
   }
   render();
+}
+
+function filter(event) {
+  mode = event.target.id;
+  filterList = [];
+
+  if (mode === "all") {
+    //전체 리스트
+    render();
+  } else if (mode === "ongoing") {
+    //진행중 리스트
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  } else if (mode === "done") {
+    //완료 리스트
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  }
 }
 
 function randomIDGenerate() {
